a4d769fc2f0b4aa2ad5413ff73e0b3db7895a141
bwebb2021 <brandonwebb@email.arizona.edu>
2023-07-21 18:19:46 -0400

    Create GridCell.java

---
+++ FindTheObjects/GridCell.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+
+public class GridCell {
+    private int x; // X coordinate of the cell on the game board
+    private int y; // Y coordinate of the cell on the game board
+    private CellState state; // The state of the cell
+
+    // Enum to represent the state of the cell
+    public enum CellState {
+        EMPTY, // Cell has no ship
+        SHIP, // Cell contains a ship
+        HIT, // Cell has been hit by a shot
+        MISS // Cell has been shot but has no ship
+    }
+
+    public GridCell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.state = CellState.EMPTY; // Initialize the cell as empty by default
+    }
+
+    // Getters and setters for the state
+    public CellState getState() {
+        return state;
+    }
+
+    public void setState(CellState state) {
+        this.state = state;
+    }
+
+    // Additional methods as needed, e.g., to check if the cell contains a ship
+}
