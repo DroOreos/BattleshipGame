37865337c2d88e50c7360eaefed67ec827268881
bwebb2021 <brandonwebb@arizona.edu>
2023-08-04 15:50:00 -0400

    More javadoc
    Added GridCellTest too

--- FindTheObjects/Gameboard.java
+++ FindTheObjects/Gameboard.java
@@ -2,12 +2,22 @@
 
 import java.util.ArrayList;
 
+/**
+ * Represents the game board for the Battleship game. The game board is a grid of cells, and ships can be placed on
+ * the board. Players can shoot at cells to attack enemy ships.
+ */
 public class Gameboard {
     private int rows; // Number of rows in the game board
     private int cols; // Number of columns in the game board
     private GridCell[][] grid; // 2D array representing the game board grid
 
     // Constructor to initialize the game board with the specified dimensions
+    /**
+     * Constructor to initialize the game board with the specified dimensions.
+     *
+     * @param rows The number of rows in the game board.
+     * @param cols The number of columns in the game board.
+     */
     public Gameboard(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
@@ -21,7 +31,15 @@ public class Gameboard {
         }
     }
 
-    // Method to place a ship on the game board
+    /**
+     * Places a ship on the game board.
+     *
+     * @param x           The x-coordinate of the top-left corner of the ship.
+     * @param y           The y-coordinate of the top-left corner of the ship.
+     * @param size        The size of the ship (number of cells it occupies).
+     * @param isHorizontal True if the ship is to be placed horizontally, false if vertically.
+     * @return True if the ship is successfully placed, false otherwise (invalid placement or overlapping ships).
+     */
     public boolean placeShip(int x, int y, int size, boolean isHorizontal) {
         // Check if the placement is valid
         if (x < 0 || y < 0 || x >= rows || y >= cols) {
@@ -63,7 +81,13 @@ public class Gameboard {
         return true;
     }
 
-    // Method to handle a shot at the specified cell coordinates (x, y)
+    /**
+     * Handles a shot at the specified cell coordinates (x, y).
+     *
+     * @param x The x-coordinate of the cell to be shot.
+     * @param y The y-coordinate of the cell to be shot.
+     * @return True if the shot is valid and successful (hit or miss), false otherwise (invalid shot).
+     */
     public boolean shoot(int x, int y) {
         // Check if the shot is valid
         if (x < 0 || y < 0 || x >= rows || y >= cols) {
@@ -82,7 +106,11 @@ public class Gameboard {
         return false; // Invalid shot (cell already hit or sunk ship)
     }
 
-    // Method to check if the player has won the game
+    /**
+     * Checks if the player has won the game.
+     *
+     * @return True if all ships on the game board are sunk (player has won), false otherwise.
+     */
     public boolean hasPlayerWon() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -94,19 +122,37 @@ public class Gameboard {
         return true; // All ships are sunk, the player has won
     }
 
-    // Getter for the grid variable
+    /**
+     * Retrieves the grid representation of the game board.
+     *
+     * @return A 2D array of GridCell objects representing the game board.
+     */
     public GridCell[][] getGrid() {
         return grid;
     }
-
+    /**
+     * Retrieves the number of rows in the game board.
+     *
+     * @return The number of rows in the game board.
+     */
     public int getRows(){
         return this.rows;
     }
-
+    /**
+     * Retrieves the number of columns in the game board.
+     *
+     * @return The number of columns in the game board.
+     */
     public int getCols(){
         return this.cols;
     }
-
+    /**
+     * Retrieves the GridCell object at the specified cell coordinates (x, y).
+     *
+     * @param x The x-coordinate of the cell.
+     * @param y The y-coordinate of the cell.
+     * @return The GridCell object at the specified coordinates.
+     */
      public GridCell getGridCell(int x, int y) {
     	return grid[x][y];
     }
--- FindTheObjects/GridCell.java
+++ FindTheObjects/GridCell.java
@@ -1,34 +1,66 @@
 //Brandon Webb
+
+
+/**
+ * Represents a single cell in the game board grid. Each cell can have one of the following states:
+ * EMPTY - The cell contains no ship.
+ * SHIP - The cell contains a part of a ship.
+ * HIT - The cell has been hit by a shot.
+ * MISS - The cell was shot at, but there was no ship.
+ */
 public class GridCell {
     private int x;
     private int y;
     private GridCell.CellState state;
-
+    /**
+     * Enumerates the possible states of a cell.
+     */
     public enum CellState {
         EMPTY,
         SHIP,
         HIT,
         MISS
     }
-
+    /**
+     * Constructs a new GridCell instance with the specified coordinates (x, y).
+     *
+     * @param x The x-coordinate of the cell on the game board.
+     * @param y The y-coordinate of the cell on the game board.
+     */
     public GridCell(int x, int y) {
         this.x = x;
         this.y = y;
         this.state = CellState.EMPTY;
     }
-
+    /**
+     * Retrieves the x-coordinate of the cell on the game board.
+     *
+     * @return The x-coordinate of the cell.
+     */
     public int getX() {
         return x;
     }
-
+    /**
+     * Retrieves the y-coordinate of the cell on the game board.
+     *
+     * @return The y-coordinate of the cell.
+     */
     public int getY() {
         return y;
     }
-
+    /**
+     * Retrieves the current state of the cell.
+     *
+     * @return The current state of the cell (EMPTY, SHIP, HIT, or MISS).
+     */
     public CellState getState() {
         return state;
     }
-
+    /**
+     * Sets the state of the cell to the specified state.
+     *
+     * @param state The new state to set for the cell (EMPTY, SHIP, HIT, or MISS).
+     */
     public void setState(CellState state) {
         this.state = state;
     }
