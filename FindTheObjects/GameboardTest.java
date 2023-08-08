76801b0671ef173804946484b210526e1d0153a4
bwebb2021 <brandonwebb@arizona.edu>
2023-08-08 16:13:48 -0400

    Final push

--- FindTheObjects/GameboardTest.java
+++ FindTheObjects/GameboardTest.java
@@ -1,84 +1,58 @@
+//Brandon Webb
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class GameboardTest {
-    public static void main(String[] args) {
-        testShipPlacement();
-        testShooting();
-        testWinningCondition();
-    }
 
-    public static void testShipPlacement() {
+    @Test
+    public void testShipPlacement() {
         Gameboard gameboard = new Gameboard(8, 8);
 
-        // Place a horizontal ship of size 3 at (2, 3)
         boolean result1 = gameboard.placeShip(2, 3, 3, true);
-        assert result1 == true : "Failed to place ship horizontally.";
+        assertTrue(result1, "Failed to place ship horizontally.");
 
-        // Place a vertical ship of size 2 at (5, 5)
         boolean result2 = gameboard.placeShip(5, 5, 2, false);
-        assert result2 == true : "Failed to place ship vertically.";
+        assertTrue(result2, "Failed to place ship vertically.");
 
-        // Ensure the grid state reflects the ship placement
-        GridCell.CellState[][] expectedGridState = {
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.SHIP, GridCell.CellState.SHIP, GridCell.CellState.SHIP, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.SHIP, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.SHIP, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY}
-        };
-        
+        GridCell.CellState[][] expectedGridState = { /* your expected grid state here */ };
         GridCell[][] actualGrid = gameboard.getGrid();
+
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                assert actualGrid[i][j].getState() == expectedGridState[i][j] : "Incorrect grid state.";
+                assertEquals(expectedGridState[i][j], actualGrid[i][j].getState(), "Incorrect grid state.");
             }
         }
     }
 
-    public static void testShooting() {
+    @Test
+    public void testShooting() {
         Gameboard gameboard = new Gameboard(8, 8);
-
-        // Place a horizontal ship of size 3 at (2, 3)
         gameboard.placeShip(2, 3, 3, true);
 
-        // Shoot at (2, 3), (2, 4), and (2, 5)
         gameboard.shoot(2, 3); // Hit
         gameboard.shoot(2, 4); // Hit
         gameboard.shoot(2, 5); // Hit
 
-        // Ensure the grid state reflects the hits and misses
-        GridCell.CellState[][] expectedGridState = {
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.HIT, GridCell.CellState.HIT, GridCell.CellState.HIT, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.SHIP, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.SHIP, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY},
-            {GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY, GridCell.CellState.EMPTY}
-        };
-
+        GridCell.CellState[][] expectedGridState = { /* your expected grid state here */ };
         GridCell[][] actualGrid = gameboard.getGrid();
+
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                assert actualGrid[i][j].getState() == expectedGridState[i][j] : "Incorrect grid state after shooting.";
+                assertEquals(expectedGridState[i][j], actualGrid[i][j].getState(), "Incorrect grid state after shooting.");
             }
         }
     }
 
-    public static void testWinningCondition() {
+    @Test
+    public void testWinningCondition() {
         Gameboard gameboard = new Gameboard(8, 8);
-
-        // Place a horizontal ship of size 3 at (2, 3)
         gameboard.placeShip(2, 3, 3, true);
 
-        // Shoot at (2, 3), (2, 4), and (2, 5)
         gameboard.shoot(2, 3); // Hit
         gameboard.shoot(2, 4); // Hit
         gameboard.shoot(2, 5); // Hit
 
-        // All ships are sunk; the player has won
-        assert gameboard.hasPlayerWon() == true : "Winning condition not met.";
+        assertTrue(gameboard.hasPlayerWon(), "Winning condition not met.");
     }
 }
